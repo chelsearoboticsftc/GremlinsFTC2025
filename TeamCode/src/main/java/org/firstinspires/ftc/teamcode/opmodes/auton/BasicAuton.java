80f864d71a731e6916ea8a30befedffdfb670556
Bryan Johnson <d.bryan.johnson@gmail.com>
2025-10-22 19:08:17 -0400

    add basic auton

---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/opmodes/auton/BasicAuton.java
@@ -0,0 +1,30 @@
+package org.firstinspires.ftc.teamcode.opmodes.auton;
+
+import com.acmerobotics.roadrunner.Pose2d;
+import com.acmerobotics.roadrunner.ftc.Actions;
+import com.qualcomm.robotcore.eventloop.opmode.Autonomous;
+import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
+
+import org.firstinspires.ftc.teamcode.MecanumDrive;
+
+@Autonomous(name = "Drive Forward 10 Feet", group = "Linear OpMode")
+public class BasicAuton extends LinearOpMode {
+    @Override
+    public void runOpMode() throws InterruptedException {
+        // Initialize MecanumDrive. The starting pose is (0, 0) with a 0-degree heading.
+        MecanumDrive drive = new MecanumDrive(hardwareMap, new Pose2d(0, 0, 0));
+
+        // Wait for the driver to press start
+        waitForStart();
+
+        if (isStopRequested()) return;
+
+        // Road Runner uses inches, so we convert 10 feet to 120 inches.
+        // We create an "action" to drive forward (along the X-axis) by 120 inches.
+        Actions.runBlocking(
+                drive.actionBuilder(new Pose2d(0, 0, 0))
+                        .lineToX(120)
+                        .build()
+        );
+    }
+}
