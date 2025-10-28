41d6fcbebd08855be0ef0a0541134b706be43642
cboland0229 <cboland0229@gmail.com>
2025-10-28 19:17:22 -0400

    checkpoint for driver testing

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/opmodes/auton/basicautondrive.java
+++
@@ -1,7 +0,0 @@
-package org.firstinspires.ftc.teamcode.opmodes.auton;
-
-import com.qualcomm.robotcore.eventloop.opmode.Disabled;
-
-@Disabled
-public class basicautondrive {
-}
--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/opmodes/teleop/TestForKirk.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/opmodes/teleop/TestForKirk.java
@@ -8,6 +8,7 @@ import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
 import com.qualcomm.robotcore.hardware.DcMotorEx;
 import com.qualcomm.robotcore.hardware.Servo;
 
+import org.firstinspires.ftc.robotcore.internal.camera.delegating.DelegatingCaptureSequence;
 import org.firstinspires.ftc.teamcode.MecanumDrive;
 
 
@@ -17,7 +18,7 @@ public class TestForKirk extends LinearOpMode {
     public void runOpMode() throws InterruptedException {
         MecanumDrive drive = new MecanumDrive(hardwareMap, new Pose2d(0,0,0));
         DcMotorEx intake = hardwareMap.get(DcMotorEx.class, "intake");
-       // DcMotorEx shooterLeft = hardwareMap.get(DcMotorEx.class, "shooterLeft");
+        DcMotorEx shooterLeft = hardwareMap.get(DcMotorEx.class, "shooterLeft");
         DcMotorEx shooterRight = hardwareMap.get(DcMotorEx.class, "shooterRight");
         Servo upperLeftGate = hardwareMap.get(Servo.class, "upperLeftGate");
         Servo upperRightGate = hardwareMap.get(Servo.class, "upperRightGate");
@@ -25,7 +26,7 @@ public class TestForKirk extends LinearOpMode {
         boolean shooterOn = false;
         waitForStart();
 
-        while(opModeIsActive()){
+        while(opModeIsActive()) {
 
             double speedFactor = 1 - gamepad1.right_trigger + 0.2;
             drive.setDrivePowers(
@@ -37,39 +38,50 @@ public class TestForKirk extends LinearOpMode {
                             -gamepad1.right_stick_x
                     )
             );
-
-
-            if (this.gamepad2.bWasPressed()) {
-                if (!shooterOn) {
-                    shooterRight.setPower(-1);
-                    shooterOn = true;
+            if (this.gamepad2.y) {
+//                shooterRight.setPower(-1);
+                shooterLeft.setPower(-1);
+            }
+            else {
+                if (this.gamepad2.bWasPressed()) {
+                    shooterOn = !shooterOn;
+                }
+                if (shooterOn) {
+//                    shooterRight.setPower(1);
+                    shooterLeft.setPower(1);
                 } else {
-                    shooterRight.setPower(0);
-                    shooterOn = false;
+//                    shooterRight.setPower(0);
+                    shooterLeft.setPower(0);
                 }
             }
+
 
             if (this.gamepad2.a)
                 intake.setPower(-1);
             else
-                intake.setPower(0);
+
+                if (this.gamepad2.x)
+                    intake.setPower(1);
+                else
+                    intake.setPower(0);
 
 
-            if (gamepad2.left_bumper){
-                upperLeftGate.setPosition(0.65);
+            if (gamepad2.left_bumper) {
+                upperLeftGate.setPosition(0.5);
             }
 
-            if(gamepad2.left_trigger > 0.1){
+            if (gamepad2.left_trigger > 0.1) {
                 upperLeftGate.setPosition(1);
             }
 
-            if (gamepad2.right_bumper){
+            if (gamepad2.right_bumper) {
                 upperRightGate.setPosition(0.4);
             }
 
-            if(gamepad2.right_trigger > 0.1){
+            if (gamepad2.right_trigger > 0.1) {
                 upperRightGate.setPosition(0.0);
             }
+
             this.telemetry.addData("left Servo", upperLeftGate.getPosition());
             this.telemetry.addData("right Servo", upperRightGate.getPosition());
             this.telemetry.update();
