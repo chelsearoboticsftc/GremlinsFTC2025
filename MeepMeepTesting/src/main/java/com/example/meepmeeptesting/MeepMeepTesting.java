08ed2314c8491fe964fa53bf2229cffd82a0bf4a
Bryan Johnson <d.bryan.johnson@gmail.com>
2025-11-19 11:20:47 -0500

    update meepmeep version to use RR 1.0

--- MeepMeepTesting/src/main/java/com/example/meepmeeptesting/MeepMeepTesting.java
+++ MeepMeepTesting/src/main/java/com/example/meepmeeptesting/MeepMeepTesting.java
@@ -1,12 +1,10 @@
 package com.example.meepmeeptesting;
 
-import com.acmerobotics.roadrunner.geometry.Pose2d;
-
-import org.rowlandhall.meepmeep.MeepMeep;
-import org.rowlandhall.meepmeep.roadrunner.DefaultBotBuilder;
-import org.rowlandhall.meepmeep.roadrunner.DriveShim;
-import org.rowlandhall.meepmeep.roadrunner.entity.RoadRunnerBotEntity;
-import org.rowlandhall.meepmeep.roadrunner.trajectorysequence.TrajectorySequence;
+import com.acmerobotics.roadrunner.Pose2d;
+import com.acmerobotics.roadrunner.Vector2d;
+import com.noahbres.meepmeep.MeepMeep;
+import com.noahbres.meepmeep.roadrunner.DefaultBotBuilder;
+import com.noahbres.meepmeep.roadrunner.entity.RoadRunnerBotEntity;
 
 import java.awt.Image;
 import java.io.File;
@@ -16,21 +14,6 @@ import javax.imageio.ImageIO;
 
 
 public class MeepMeepTesting {
-
-    public static TrajectorySequence builder(DriveShim drive, double rowY) {
-        final double gridSize = 23.5;
-        final Pose2d startingPos = new Pose2d(-2.1 * gridSize, 2.1 * gridSize, Math.toRadians(-35));
-        return drive.trajectorySequenceBuilder(startingPos)
-            .splineToLinearHeading(
-                    new Pose2d(-2. * gridSize, rowY + 2, Math.toRadians(-160)),
-                    Math.toRadians(-160)
-            )
-            // reverse back to starting pose
-            .setReversed(true)
-            .splineTo(startingPos.vec(), startingPos.getHeading() + Math.PI)
-            .build();
-    }
-
     public static void main(String[] args) {
         MeepMeep meepMeep = new MeepMeep(800);
 
@@ -38,9 +21,20 @@ public class MeepMeepTesting {
         RoadRunnerBotEntity myBot = new DefaultBotBuilder(meepMeep)
                 .setDimensions(16, 16)
                 .setConstraints(60, 60, Math.toRadians(180), Math.toRadians(180), 15)
-                .followTrajectorySequence(drive -> MeepMeepTesting.builder(drive, .5 * gridSize));
-        // row positions: 0.5, -0.5, and -1.5 * gridSize
+                .build();
 
+        final Pose2d startingPos = new Pose2d(-2.1 * gridSize, 2.1 * gridSize, Math.toRadians(-35));
+        final double rowY = -1.5 * gridSize;
+        myBot.runAction(myBot.getDrive().actionBuilder(startingPos)
+                .splineTo(
+                        new Vector2d(-2.1 * gridSize, rowY + 2),
+                        Math.toRadians(-180)
+                )
+                // reverse back to starting pose
+                .setReversed(true)
+                .splineTo(startingPos.position, startingPos.heading.plus(Math.toRadians(180)))
+                .build()
+        );
 
         Image img = null;
         try {
