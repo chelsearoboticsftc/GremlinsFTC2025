2fc437092f5569272d95bd3d438c5747971dfe53
Bryan Johnson <d.bryan.johnson@gmail.com>
2025-12-08 21:10:21 -0500

    fine tune auton

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/opmodes/auton/RedTokyoDrift.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/opmodes/auton/RedTokyoDrift.java
@@ -47,7 +47,7 @@ public class RedTokyoDrift extends LinearOpMode {
         // load field artifacts
         Actions.runBlocking(slurpArtifacts(drive, 0.5 * gridSize));
         shoot(shooter);
-        Actions.runBlocking(slurpArtifacts(drive, -0.6 * gridSize));
+        Actions.runBlocking(slurpArtifacts(drive, -0.4 * gridSize));
         shoot(shooter);
 
         // leave line
@@ -63,19 +63,10 @@ public class RedTokyoDrift extends LinearOpMode {
         intake.setPower(0);
     }
 
-    private void runAction(MecanumDrive drive, Limelight limelight, Action trajectoryAction) {
-        boolean running = true;
-        while (opModeIsActive() && running) {
-            drive.updatePoseEstimate();
-            limelight.updatePose(drive, telemetry);
-            running = trajectoryAction.run(new TelemetryPacket());
-        }
-    }
-
     private Action slurpArtifacts(MecanumDrive drive, double posY) {
         double wallX = (2.0 * gridSize);
         if (posY < 0.2 * gridSize){
-            wallX = (2.25 * gridSize);
+            wallX = (2.7 * gridSize);
         }
         return drive.actionBuilder(drive.localizer.getPose())
                 .splineToLinearHeading(
